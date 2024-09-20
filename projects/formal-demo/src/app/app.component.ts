b85c2c13389998e7cb32174c03d81dad738582c4
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-09-20 12:10:45 -0400

    Update example to use nested forms

--- projects/formal-demo/src/app/app.component.ts
+++ projects/formal-demo/src/app/app.component.ts
@@ -5,13 +5,10 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
-import {
-  form,
-  formalDirectives,
-} from 'formal';
+import { form, formalDirectives } from 'formal';
+import { disabledIf } from '../../../formal/src/lib/form/state/disabled';
 import { CustomFormFieldNumberComponent } from '../custom-form-field-number/custom-form-field-number.component';
 import { CustomValueAccessorNumberFieldComponent } from '../custom-value-accessor-number-field/custom-value-accessor-number-field.component';
-import { disabledIf } from '../../../formal/src/lib/form/state/disabled';
 
 @Component({
   selector: 'app-root',
@@ -33,13 +30,20 @@ import { disabledIf } from '../../../formal/src/lib/form/state/disabled';
   ],
 })
 export class AppComponent {
-  readonly disabledForm = form('I\'m Disabled', [disabledIf('Disabled Demo')]);
+  form = (() => {
+    const age = form(42);
 
-  form = form({
-    name: 'Sweeney Todd',
-    age: 42,
-    gender: 'male',
-  });
+    return form({
+      name: 'Sweeney Todd',
+      age: age,
+      gender: 'male',
+      partner: form('', [
+        disabledIf(() =>
+          age() < 18 ? 'You must be 18 or older to have a partner' : null,
+        ),
+      ]),
+    });
+  })();
 
   save() {
     alert(JSON.stringify(this.form(), null, 2));
