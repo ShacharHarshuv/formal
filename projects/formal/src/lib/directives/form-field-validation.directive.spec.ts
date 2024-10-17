2664bed8c23eb110347fac783390eab191b70eb9
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-10-17 14:09:08 -0400

    Error indication classes: tests

--- projects/formal/src/lib/directives/form-field-validation.directive.spec.ts
+++ projects/formal/src/lib/directives/form-field-validation.directive.spec.ts
@@ -1,8 +1,54 @@
-import { FormFieldValidationDirective } from './form-field-validation.directive';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Form, form, formalDirectives, withValidators } from 'formal';
+
+function createComponentFixtureWithForm(form: Form<string>) {
+  @Component({
+    selector: 'app-root',
+    template: ` <input [formField]="myForm" type="text" /> `,
+  })
+  class AppComponent {
+    readonly myForm = form;
+  }
+
+  TestBed.configureTestingModule({
+    imports: [
+      MatFormFieldModule,
+      MatInputModule,
+      formalDirectives,
+      NoopAnimationsModule,
+    ],
+    declarations: [AppComponent],
+  }).compileComponents();
+
+  const fixture = TestBed.createComponent(AppComponent);
+  TestBed.flushEffects();
+  fixture.detectChanges();
+  TestBed.flushEffects();
+  fixture.detectChanges();
+
+  return [fixture, fixture.nativeElement.querySelector('input')];
+}
 
 describe('FormFieldValidationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FormFieldValidationDirective();
-    expect(directive).toBeTruthy();
+  it('valid', () => {
+    const [fixture, inputElm] = createComponentFixtureWithForm(
+      form('Hello, World!'),
+    );
+    expect(inputElm.classList).toContain('ng-valid');
+    expect(inputElm.classList).not.toContain('ng-invalid');
+    expect(inputElm.classList).not.toContain('ng-pending');
+  });
+
+  it('invalid', () => {
+    const [fixture, inputElm] = createComponentFixtureWithForm(
+      form('', [withValidators(() => 'error!')]),
+    );
+    expect(inputElm.classList).toContain('ng-invalid');
+    expect(inputElm.classList).not.toContain('ng-valid');
+    expect(inputElm.classList).not.toContain('ng-pending');
   });
 });
